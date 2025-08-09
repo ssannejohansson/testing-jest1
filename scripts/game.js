108019f76dbc2511ebf52c3e5063c85fb35a9cac
ssannejohansson <sannej0hansson@hotmail.com>
2025-08-09 09:36:50 +0200

    Add game functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,32 +1,86 @@
 let game = {
-    score: 0, 
+    score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"],
+    lastButton: "",
+    turnInProgress: false,
 }
 
-function newGame () {
-
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listner") !== "true") {
+            circle.addEventListener("click", (e) => {
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    lightsOn(move);
+                    game.playerMoves.push(move);
+                    playerTurn();
+                }
+            });
+            circle.setAttribute("data-listner", "true");
+        }
+    }
+    showScore();
+    addTurn();
 }
 
 function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+};
 
-}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+};
 
 function showTurns() {
-
-}
-
-function lightsOn() {
-
+    game.turnInProgress = true;
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+            game.turnInProgress = false; 
+        }
+    }, 800)
 }
 
 function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length == game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+};
 
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 }
 
-function showScore () {
-
-}
-
-module.exports = { game }; 
+module.exports = {
+    game,
+    newGame,
+    showScore,
+    addTurn,
+    lightsOn,
+    showTurns,
+    playerTurn
+};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,6 +1,9 @@
 const {
-    game
+    game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn
 } = require("../game.js");
+
+jest.spyOn(window, "alert").mockImplementation(() => { });
+
 
 beforeAll(() => {
     let fs = require("fs");
@@ -24,4 +27,93 @@ describe("game objects contains correct keys", () => {
     test("choices contain corret ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]); 
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", () => {
+        expect("turnInProgress" in game).toBe(true); 
+    });
 });
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentCame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42"; 
+        newGame();
+    });
+    test("shoud set the game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0); 
+    });
+    test("should be one move in the computer's game array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);    
+    });
+    test("expect data-listner to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listner")).toEqual("true");
+        }
+    });
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]); 
+        lightsOn(game.currentGame[0]); 
+        expect(button.classList).toContain("light");
+    });
+    test ("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42; 
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("shoud call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
+    });
+    test("shoud toggle turnInProgress to be true", () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during computer sequence shoud fail", () => {
+        showTurns();
+        game.lastButton = ""; 
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual(""); 
+    });
+});
+
